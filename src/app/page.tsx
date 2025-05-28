f6ed9167c4940d67c090d326264422b7e8776753
Tosporn Piromwong <iff_k4@hotmail.com>
2025-05-28 15:24:37 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,13 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          src="https://media.coolism.net/rs-coolism/image/png/2abe23f9-2862-4770-aef3-2d6a7082a715-bc590b07e8.png"
-          width={300}
-          height={300}
-        />
+            <Image
+              className="dark:invert"
+              src="/logo.svg"
+              alt="Vercel logomark"
+              width={200}
+              height={200}
+            />
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">
              11111. Get started by editing{" "}
